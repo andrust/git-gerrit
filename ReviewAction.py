0dc305a72217940209b36248cb495282d5ca961d
andrust <tamas.andrus@gmail.com>
2019-03-11 10:34:14 +0100

    added hotkeys for review dialog
    new hotkeys:
    
    'f2' :      cr_approved
    'f1' :      cr_suggested
    '0' :       cr_neutral
    'meta f1' : cr_disliked
    'meta f2' : cr_denied

--- ReviewAction.py
+++ ReviewAction.py
@@ -8,6 +8,7 @@ class ReviewAction(urwid.WidgetWrap):
         self.cview = chageview
         self.labels = {"Verified": 0, "Code-Review": 0}
         self.cview.add_hotkey("R", self.open_popup)
+        self.radiomap = {}
         super(ReviewAction, self).__init__(Button("Review", "button", self.open_popup))
 
     def open_popup(self, w=None):
@@ -16,6 +17,7 @@ class ReviewAction(urwid.WidgetWrap):
                 self.labels = r['approvals']
                 break
         content = []
+        self.radiomap = {}
         for labelname, valid_values in self.cview.change['permitted_labels'].iteritems():
             lst = []
             rgroup = []
@@ -24,12 +26,21 @@ class ReviewAction(urwid.WidgetWrap):
                 default = False
                 if int(i) == int(self.labels[labelname]):
                     default = True
-                lst.append(urwid.RadioButton(rgroup, i, state=default, on_state_change=self.set_label, user_data=(labelname, i)))
+                radio = urwid.RadioButton(rgroup, i, state=default, on_state_change=self.set_label, user_data=(labelname, i))
+                lst.append(radio)
+                self.radiomap[i] = radio
             content.append(urwid.ListBox(lst))
         col = urwid.BoxAdapter((urwid.Columns(content)), 7)
         apply_ = urwid.Padding(urwid.Button("Apply", self.send_labels), 'center', 9)
         layout = urwid.ListBox([col, apply_])
-        self.cview.main.open_popup(InputHandler(urwid.LineBox(layout), {'ctrl ^' : self.send_labels}), 10, 26)
+        hotkeys = { 'ctrl ^' : self.send_labels,
+                    'f2' : self.cr_approved,
+                    'f1' : self.cr_suggested,
+                    '0' : self.cr_neutral,
+                    'meta f1' : self.cr_disliked,
+                    'meta f2' : self.cr_denied
+                  }
+        self.cview.main.open_popup(InputHandler(urwid.LineBox(layout), hotkeys), 10, 26)
 
     def send_labels(self, button=None):
         if 0 < len(self.labels.keys()):
@@ -41,3 +52,18 @@ class ReviewAction(urwid.WidgetWrap):
         if state:
             label, vote = value
             self.labels[label] = int(vote)
+
+    def cr_approved(self):
+        self.radiomap['+2'].set_state(True)
+
+    def cr_suggested(self):
+        self.radiomap['+1'].set_state(True)
+
+    def cr_neutral(self):
+        self.radiomap[' 0'].set_state(True)
+
+    def cr_disliked(self):
+        self.radiomap['-1'].set_state(True)
+
+    def cr_denied(self):
+        self.radiomap['-2'].set_state(True)
